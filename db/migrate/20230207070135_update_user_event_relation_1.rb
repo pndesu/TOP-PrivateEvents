376755f261a58e992ab3db916d62db87ee5a9e30
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-07 18:13:09 +1100

    Add devise user model

---
+++ db/migrate/20230207070135_update_user_event_relation_1.rb
@@ -0,0 +1,5 @@
+class UpdateUserEventRelation1 < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :events, :creator, foreign_key: {to_table: :users}
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_063926) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_070135) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
