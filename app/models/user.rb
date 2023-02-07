0372b70d4faa6c53e56b81543ced6e767b565d66
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-07 20:43:36 +1100

    Add invitation through model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :created_events, foreign_key: "creator_id", class_name: "Post"
+  has_many :created_events, foreign_key: :creator_id, class_name: "Post"
+  has_many :attended_events, through: :invitations
+  has_many :invitations, foreign_key: :attendee_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_070135) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_092341) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_070135) do
     t.datetime "updated_at", null: false
     t.integer "creator_id"
     t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_invitations_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_invitations_on_attendee_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_070135) do
   end
 
   add_foreign_key "events", "users", column: "creator_id"
+  add_foreign_key "invitations", "events", column: "attended_event_id"
+  add_foreign_key "invitations", "users", column: "attendee_id"
 end
