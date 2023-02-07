2b8484d50779000ec6d737e774bb18a982b07f56
pndesu <tranvanphucnguyen1@gmail.com>
2023-02-07 18:34:12 +1100

    Add create event logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,23 @@
 class EventsController < ApplicationController
     def index
     end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.build_event(event_params)
+        if @event.save
+            redirect_to user_path
+            flash[:notice] = "Event created succesfully!"
+        else
+            render :new, status: unprocessable_entity
+        end
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:date, :location)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "events#index"
+  resources :events, only: [:index, :new, :create]
+  resources :users, only: [:show]
 end
